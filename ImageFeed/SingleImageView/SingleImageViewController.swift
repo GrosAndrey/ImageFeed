4b2ca915b699387743a42bea2524c5ad6f97e9f8
gros_andrey <andrey_groshev@me.com>
2026-02-13 09:26:16 +0300

    feat: доработка экрана SingleImageView

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Грошев on 13.02.2026.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+}
