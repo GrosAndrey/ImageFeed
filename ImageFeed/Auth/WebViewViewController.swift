3d6149e2d67af134c74d2d553551daf1cbef513e
gros_andrey <andrey_groshev@me.com>
2026-02-28 12:20:35 +0300

    feat: добавлена подписка на обновление progressView

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -14,7 +14,7 @@ enum WebViewConstants {
 
 final class WebViewViewController: UIViewController {
     @IBOutlet weak private var webView: WKWebView!
-    @IBOutlet weak private var UIProgressView: UIProgressView!
+    @IBOutlet weak private var progressView: UIProgressView!
     
     weak var delegate: WebViewViewControllerDelegate?
     
@@ -24,6 +24,50 @@ final class WebViewViewController: UIViewController {
         webView.navigationDelegate = self
         
         loadAuthView()
+        updateProgress()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        webView.addObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            options: .new,
+            context: nil)
+        
+        updateProgress()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        webView.removeObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress)
+        )
+        
+        if isMovingFromParent {
+            delegate?.webViewViewControllerDidCancel(self)
+        }
+    }
+    
+    override func observeValue(
+        forKeyPath keyPath: String?,
+        of object: Any?,
+        change: [NSKeyValueChangeKey : Any]?,
+        context: UnsafeMutableRawPointer?
+    ) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            updateProgress()
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+    
+    private func updateProgress() {
+        progressView.progress = Float(webView.estimatedProgress)
+        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
     }
     
     private func loadAuthView() {
